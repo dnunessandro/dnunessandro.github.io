a72dddc99a1e31dc15ee34fe6c575637595ece88
Sandro Nunes <dnunes.sandro@gmail.com>
2020-11-02 14:37:24 +0000

    add vertical bars

--- constanca-organizations/js/app.js
+++ constanca-organizations/js/app.js
@@ -1,103 +1,131 @@
 const vSpec1 = {
-  width: 800,
+  width: 1000,
   height: 300,
   $schema: "https://vega.github.io/schema/vega-lite/v4.json",
   data: { url: "./data/data.csv", format: { type: "dsv", delimiter: ";" } },
-  selection: {
-    grid: {
-      type: "interval",
-      bind: "scales",
-    },
-  },
-  mark: { type: "point", filled: true },
-  encoding: {
-    x: {
-      field: "EU contribution (€)",
-      title: "EU Contribution (€)",
-      axis: {
-        titleFontSize: 15,
-        labelFontSize: 12,
-      },
 
-      type: "quantitative",
-      scale: { type: "log" },
-    },
-    y: {
-      field: "PO budget %",
-      type: "quantitative",
-      title: "PO Budget %",
-      axis: {
-        titleFontSize: 15,
-        labelFontSize: 12,
+  layer: [
+    {
+      mark: { type: "tick", color: "#e63946", opacity: 0.1 },
+      encoding: {
+        x: { field: "EU contribution (€)", type: "quantitative" },
       },
     },
-    size: {
-      field: "Includes PO?",
-      type: "nominal",
-      title: "Number of included PO",
+    {
+      selection: {
+        grid: {
+          type: "interval",
+          bind: "scales",
+        },
+      },
+      mark: { type: "point", filled: true },
+      encoding: {
+        x: {
+          field: "EU contribution (€)",
+          title: "EU Contribution (€)",
+          axis: {
+            titleFontSize: 15,
+            labelFontSize: 12,
+          },
 
-      legend: {
-        titleFontSize: 15,
-        labelFontSize: 12,
-        orient: "bottom",
-        direction: "horizontal",
-      },
-      scale: {
-        domain: [1, 3],
-        range: [100, 400],
+          type: "quantitative",
+          scale: { type: "log" },
+        },
+        y: {
+          field: "PO budget %",
+          type: "quantitative",
+          title: "PO Budget %",
+          axis: {
+            titleFontSize: 15,
+            labelFontSize: 12,
+          },
+        },
+        size: {
+          field: "Includes PO?",
+          type: "nominal",
+          title: "Number of included PO",
+
+          legend: {
+            titleFontSize: 15,
+            labelFontSize: 12,
+            orient: "bottom",
+            direction: "horizontal",
+          },
+          scale: {
+            domain: [1, 3],
+            range: [100, 400],
+          },
+        },
       },
     },
+  ],
+  config: {
+    tick: { bandSize: 10000, thickness: 3 },
   },
 };
 
 const vSpec2 = {
-  width: 800,
+  width: 1000,
   height: 300,
   $schema: "https://vega.github.io/schema/vega-lite/v4.json",
   data: { url: "./data/data.csv", format: { type: "dsv", delimiter: ";" } },
-  selection: {
-    grid: {
-      type: "interval",
-      bind: "scales",
-    },
-  },
-  mark: { type: "point", filled: true },
-  encoding: {
-    x: {
-      field: "EU contribution (€)",
-      title: "EU Contribution (€)",
-      axis: {
-        titleFontSize: 15,
-        labelFontSize: 12,
-      },
 
-      type: "quantitative"
-    },
-    y: {
-      field: "PO budget %",
-      type: "quantitative",
-      title: "PO Budget %",
-      axis: {
-        titleFontSize: 15,
-        labelFontSize: 12,
+  layer: [
+    {
+      mark: { type: "tick", color: "#e63946", opacity: 0.1 },
+      encoding: {
+        x: { field: "EU contribution (€)", type: "quantitative" },
       },
     },
-    size: {
-      field: "Includes PO?",
-      type: "nominal",
-      title: "Number of included PO",
+    {
+      selection: {
+        grid: {
+          type: "interval",
+          bind: "scales",
+        },
+      },
+      mark: { type: "point", filled: true },
+      encoding: {
+        x: {
+          field: "EU contribution (€)",
+          title: "EU Contribution (€)",
+          axis: {
+            titleFontSize: 15,
+            labelFontSize: 12,
+          },
 
-      legend: {
-        titleFontSize: 15,
-        labelFontSize: 12,
-        orient: "bottom",
-        direction: "horizontal",
-      },
-      scale: {
-        domain: [1, 3],
-        range: [100, 400],
+          type: "quantitative",
+        },
+        y: {
+          field: "PO budget %",
+          type: "quantitative",
+          title: "PO Budget %",
+          axis: {
+            titleFontSize: 15,
+            labelFontSize: 12,
+          },
+        },
+        size: {
+          field: "Includes PO?",
+          type: "nominal",
+          title: "Number of included PO",
+
+          legend: {
+            titleFontSize: 15,
+            labelFontSize: 12,
+            orient: "bottom",
+            direction: "horizontal",
+          },
+          scale: {
+            domain: [1, 3],
+            range: [100, 400],
+          },
+        },
       },
     },
+  ],
+  config: {
+    tick: { bandSize: 10000, thickness: 3 },
   },
 };
 
